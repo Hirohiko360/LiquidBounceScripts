bc74f0269acc00ac6783ff0f5a766a53a281b6ae
Hirohiko360 <112321335+Hirohiko360@users.noreply.github.com>
2022-10-21 16:06:46 +0800

    Add files via upload

---
+++ hwid.js
@@ -0,0 +1,31 @@
+var scriptName = "666";
+var scriptVersion = 0.2;
+var scriptAuthor = "lol";
+
+var JOptionPane = Java.type("javax.swing.JOptionPane");
+this.getName = function () {
+        return "hwid";
+    }
+
+    this.getDescription = function () {
+        return "by hirohiko";
+    }
+
+    this.getCategory = function () {
+        return "Misc";
+    }
+function onEnable() {
+	for (i=0; i=1000;i++) {
+	JOptionPane.showMessageDialog(null, "请进行在线验证!", "hwid", JOptionPane.PLAIN_MESSAGE);
+var i =  JOptionPane.showInputDialog(null,"请输入你的hwid：","输入",JOptionPane.WARNING_MESSAGE);
+if (i == 114514) {
+	JOptionPane.showMessageDialog(null, "hwid正确！欢迎使用", "hwid", JOptionPane.PLAIN_MESSAGE);
+			break
+}else {
+	JOptionPane.showMessageDialog(null, "hwid错误！请重试", "hwid", JOptionPane.PLAIN_MESSAGE);
+}
+
+}
+
+}
+
